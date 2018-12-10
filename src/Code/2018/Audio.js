1b6ab7629366e6cc01e9b89720a2024b530b1b66
bigRubbish <984388932@qq.com>
2018-12-10 15:42:18 +0800

    删除2018

---
+++ src/Code/2018/Audio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import yuanshengRouter from "../yuanshengRouter.js";
+
+class Start extends React.Component{
+	render(){
+		yuanshengRouter();
+		return (
+			<div></div>
+		)
+	}
+}
+
+export default Start;
